329d3e445d3be0e6f5b097a499b3353f74d4b404
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2022-11-27 00:12:09 +0300

    ırepository

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> Table { get; set; }
+    }
+}
