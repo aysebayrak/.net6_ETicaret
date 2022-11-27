cfded007de748b7af2d59812c0ad832a2fb3329d
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2022-11-27 12:56:04 +0300

    ırepository implement

--- ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IRepository.cs
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ETicaretAPI.Domain.Entities.Common;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> where T : BaseEntity
     {
-        DbSet<T> Table { get; set; }
+        DbSet<T> Table { get; }
     }
 }
